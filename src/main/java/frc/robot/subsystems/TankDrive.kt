6255601a9d96c66dbda3492a90a53f1fd9eccf8a
s-s25 <115739101+cassiopeia3@users.noreply.github.com>
2025-03-12 20:19:47 -0400

    make go

--- src/main/java/frc/robot/subsystems/TankDrive.kt
+++ src/main/java/frc/robot/subsystems/TankDrive.kt
@@ -1,20 +1,31 @@
 package frc.robot.subsystems
 
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX
+import kotlin.math.max
+import kotlin.math.abs
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.wpilibj.Joystick
+import com.revrobotics.CANSparkMax
+import com.revrobotics.CANSparkLowLevel.MotorType.kBrushless
 
-class TankDrive : SubsystemBase() {
-    private val frontLeftWheel = PWMVictorSPX()
-    private val frontRightWheel = PWMVictorSPX()
-    private val backLeftWheel = PWMVictorSPX()
-    private val backRightWheel = PWMVictorSPX()
-    fun rightSideSpeed(speed : Double){
-        frontRightWheel.set()
-        backRightWheel.set()
+object TankDrive : SubsystemBase() {
+    private val frontleft=CANSparkMax(22,kBrushless)
+    private val frontright=CANSparkMax(21,kBrushless)
+    private val backleft =CANSparkMax(23,kBrushless)
+    private val backright =CANSparkMax(17,kBrushless)
+
+    private val stick=Joystick(0)
+
+    fun gojustgogoddammit(){
+        var x=stick.getX()
+        var y=stick.getY()
+        var lout=x-y
+        var rout=-x-y
+        var max=max(abs(lout),abs(rout))
+        lout*=max
+        rout*=max
+        frontleft.set(lout)
+        backleft.set(lout)
+        frontright.set(rout)
+        backright.set(rout)
     }
-    fun leftSideSpeed(speed : Double){
-
-    }
-
-
 }
